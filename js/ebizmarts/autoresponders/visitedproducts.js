82de4dc3842411147efa57bb775027411016a5c2
Swapna Palaniswamy <swapna@aligent.com.au>
2013-10-01 11:58:23 +0930

    Fix the ajax request by removing index.php

--- js/ebizmarts/autoresponders/visitedproducts.js
+++ js/ebizmarts/autoresponders/visitedproducts.js
@@ -6,7 +6,8 @@ function markVisited(productID) {
 (function() {
     var cb = function() {
         var productID = $$('input[name^=product]').first().value;
-        new Ajax.Request('../index.php/ebizautoresponder/autoresponder/getVisitedProductsConfig?product_id='+productID, { method:'get', onSuccess: function(transport){
+        //Making a fix based on the solution proposed by the support team in  http://ebizmarts.com/forums/topics/view/8135
+        new Ajax.Request('../ebizautoresponder/autoresponder/getVisitedProductsConfig?product_id='+productID, { method:'get', onSuccess: function(transport){
                 if(transport.responseJSON.time > -1) {
                     markVisited.delay(transport.responseJSON.time,productID);
                 }
